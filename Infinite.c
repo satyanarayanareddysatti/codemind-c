bac5d76d8328f3b23f04583466ed832f2ac22a34
satyanarayanareddysatti <119919492+satyanarayanareddysatti@users.noreply.github.com>
2022-12-20 21:29:43 +0530

    #include<stdio.h> int main() {     int m,c;     //scanf("%d%d",&m,&n);     while(m!=-1)     {         scanf("%d",&m);         if((m*m)!=1)         printf("%d ",(m*m));     }
    }

---
+++ Infinite.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int m,c;
+    //scanf("%d%d",&m,&n);
+    while(m!=-1)
+    {
+        scanf("%d",&m);
+        if((m*m)!=1)
+        printf("%d
+",(m*m));
+    }
+    
+}
