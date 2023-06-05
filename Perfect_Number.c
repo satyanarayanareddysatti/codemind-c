0e2229907fe81aac64df58f21bda0fd7abe04da2
satyanarayanareddysatti <119919492+satyanarayanareddysatti@users.noreply.github.com>
2023-06-05 13:58:59 +0530

    #include<stdio.h> int main() {     int n,s=0;     scanf("%d",&n);     for(int i=1;i<n;i++)     {         if(n%i==0)         {             s=s+i;        }             }             if(s==n)             {        printf("True");             }             else             {                 printf("False");                 } }

--- Perfect_Number.c
+++ Perfect_Number.c
@@ -1,21 +1,19 @@
 #include<stdio.h>
 int main()
 {
-    int n,i,s=0;
-    scanf("%d",&n);
-    for(i=1;i<=n/2;i++)
+    int n,s=0;  
+    scanf("%d",&n);  
+    for(int i=1;i<n;i++)    
     {
-        if(n%i==0)
+        if(n%i==0) 
         {
-            s=s+i;
-        }
-    }
-    if(n==s)
-    {
-        printf("True");
-    }
-    else
-    {
-        printf("False");
-    }
+            s=s+i;        }
+            } 
+            if(s==n)
+            {        printf("True");
+            }
+            else
+            {
+                printf("False");
+                }
 }
